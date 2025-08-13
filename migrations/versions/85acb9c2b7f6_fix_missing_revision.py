b3b727ac07063c1d83e2982b4da4cc0b93504493
Khaatir Muhammad <ciberpointeent@gmail.com>
2025-08-13 14:25:51 -0400

    Add dummy migration to fix Render DB state

---
+++ migrations/versions/85acb9c2b7f6_fix_missing_revision.py
@@ -0,0 +1,23 @@
+"""Fix missing revision
+
+Revision ID: 85acb9c2b7f6
+Revises: 5c575ec8fd3f
+Create Date: 2025-08-13 14:30:00.000000
+
+"""
+# revision identifiers, used by Alembic.
+revision = '85acb9c2b7f6'
+down_revision = '5c575ec8fd3f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # This migration is intentionally left empty.
+    # It's a placeholder to fix a broken migration history.
+    pass
+
+
+def downgrade():
+    # This migration is intentionally left empty.
+    pass
